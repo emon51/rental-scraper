b3aeb5f29804760d018c248d4afcf2f6edd193ae
emon51 <emanur11235@gmail.com>
2026-02-18 12:25:53 +0600

    Move Listing struct to models package

---
+++ models/listing.go
@@ -0,0 +1,11 @@
+package models
+
+type Listing struct {
+	Platform    string
+	Title       string
+	Price       string
+	Location    string
+	Rating      string
+	URL         string
+	Description string
+}
