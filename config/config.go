d933d9c0e2c0d86bd7fbaddaa25d1ee07d993c8f
emon51 <emanur11235@gmail.com>
2026-02-19 13:09:05 +0600

    Add multi-location configuration with city slugs and display names

--- config/config.go
+++ config/config.go
@@ -2,11 +2,17 @@ package config
 
 type Config struct {
 	BaseURL      string
+	Locations    []LocationConfig
 	MaxListings  int
 	PageTimeout  int
 	RequestDelay int
 	Headless     bool
 	DBConfig     DatabaseConfig
+}
+
+type LocationConfig struct {
+	Slug        string 
+	DisplayName string
 }
 
 type DatabaseConfig struct {
@@ -19,11 +25,22 @@ type DatabaseConfig struct {
 
 func NewConfig() *Config {
 	return &Config{
-		BaseURL:      "https://www.airbnb.com/s/Kuala-Lumpur/homes",
-		MaxListings:  10,
-		PageTimeout:  300,
+		BaseURL:     "https://www.airbnb.com/s/%s/homes",
+		MaxListings: 2,
+		PageTimeout: 600,
 		RequestDelay: 2,
-		Headless:     true,
+		Headless:    true,
+		Locations: []LocationConfig{
+			{Slug: "Kuala-Lumpur", DisplayName: "Kuala Lumpur, Malaysia"},
+			{Slug: "Bangkok", DisplayName: "Bangkok, Thailand"},
+			{Slug: "Seoul", DisplayName: "Seoul, South Korea"},
+			{Slug: "Tokyo", DisplayName: "Tokyo, Japan"},
+			{Slug: "Melbourne", DisplayName: "Melbourne, Australia"},
+			{Slug: "Sydney", DisplayName: "Sydney, Australia"},
+			{Slug: "Osaka", DisplayName: "Osaka, Japan"},
+			{Slug: "Johor-Bahru-District", DisplayName: "Johor Bahru, Malaysia"},
+			{Slug: "Busan", DisplayName: "Busan, South Korea"},
+		},
 		DBConfig: DatabaseConfig{
 			Host:     "localhost",
 			Port:     5432,
